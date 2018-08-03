df7211938a3d4f91f7bf8735f20cb9a46eaeaebe
Grégory Roussel <siriusocteam@gmail.com>
2018-08-03 15:25:48 +0200

    Add of color code for border on Intervention voucher
    Add of a color code for the border on the Intervention voucher and some comments to get more descriptive code on all sql query

--- front/configOs.php
+++ front/configOs.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+// sql config 
 $SelPlugin = "SELECT * FROM glpi_plugin_os_config";
 $ResPlugin = $DB->query($SelPlugin);
 $Plugin = $DB->fetch_assoc($ResPlugin);
@@ -10,29 +11,36 @@ $CidadePlugin = $Plugin['city'];
 $CorPlugin = $Plugin['color'];
 $CorTextoPlugin = $Plugin['textcolor'];
 $SitePlugin = $Plugin['site'];
+$CorBordPlugin = '#aaa';
+// sql ticket
 $SelTicket = "SELECT * FROM glpi_tickets WHERE id = '".$_GET['id']."'";
 $ResTicket = $DB->query($SelTicket);
 $Ticket = $DB->fetch_assoc($ResTicket);
 $OsId = $_GET['id'];
 $OsNome = $Ticket['name'];
+// sql date ticket
 $SelDataOs = "SELECT date,date_format(date, '%d/%m/%Y') AS DataOs FROM glpi_tickets WHERE id = '".$_GET['id']."'";
 $ResSelData = $DB->query($SelDataOs);
 $ResSelDataFinal = $DB->fetch_assoc($ResSelData);
 $DataOs = $ResSelDataFinal['DataOs'];
+// sql date début
 $SelDataInicial = "SELECT date,date_format(date, '%d/%m/%Y %H:%i') AS DataInicio FROM glpi_tickets WHERE id = '".$_GET['id']."'";
 $ResDataInicial = $DB->query($SelDataInicial);
 $DataInicial = $DB->fetch_assoc($ResDataInicial);
 $OsData = $DataInicial['DataInicio'];
 $OsDescricao = $Ticket['content'];
+// sql date résolution
 $SelDataFinal = "SELECT time_to_resolve,date_format(solvedate, '%d/%m/%Y %H:%i') AS DataFim FROM glpi_tickets WHERE id = '".$_GET['id']."'";
 $ResDataFinal = $DB->query($SelDataFinal);
 $DataFinal = $DB->fetch_assoc($ResDataFinal);
 $OsDataEntrega = $DataFinal['DataFim'];
 $OsSolucao = $Ticket['solution'];
+// sql utilisateur
 $SelTicketUsers = "SELECT * FROM glpi_tickets_users WHERE tickets_id = '".$OsId."'";
 $ResTicketUsers = $DB->query($SelTicketUsers);
 $TicketUsers = $DB->fetch_assoc($ResTicketUsers);
 $OsUserId = $TicketUsers['users_id'];
+// sql technicien
 $SelIdOsResponsavel = "SELECT users_id FROM glpi_tickets_users WHERE tickets_id = '".$OsId."' AND type = 2";
 $ResIdOsResponsavel = $DB->query($SelIdOsResponsavel);
 $OsResponsavel = "";
@@ -45,6 +53,7 @@ while ($IdOsResponsavel = $DB->fetch_assoc($ResIdOsResponsavel)) {
 if(strlen($OsResponsavel)>2){
 	$OsResponsavel = substr($OsResponsavel, 0, strlen($OsResponsavel)-2);
 }
+// sql date modification
 $SelAtendimento = "select max(date_format(date_mod, '%d/%m/%Y %H:%i')) as date_mod from glpi_logs where itemtype like 'Ticket' and id_search_option=12 and new_value=15 and items_id=".$OsId;
 $ResDtAtendimento = $DB->query($SelAtendimento);
 if($ResDtAtendimento){
@@ -53,6 +62,7 @@ if($ResDtAtendimento){
 		$OsDataAtendimento = $dtatend['date_mod'];
 	}	
 }
+// sql entité
 $EntidadeId = $Ticket['entities_id'];
 $SelEmpresa = "SELECT * FROM glpi_entities WHERE id = '".$EntidadeId."'";
 $ResEmpresa = $DB->query($SelEmpresa);
@@ -63,17 +73,21 @@ $EntidadeEndereco = $Empresa['address'];
 $EntidadeEmail = $Empresa['email'];
 $EntidadePhone = $Empresa['phonenumber'];
 $EntidadeCnpj = $Empresa['comment'];
+// sql email
 $SelEmail = "SELECT * FROM glpi_useremails WHERE users_id = '".$OsUserId."'";
 $ResEmail = $DB->query($SelEmail);
 $Email = $DB->fetch_assoc($ResEmail);
 $UserEmail = $Email['email'];
+// sql coûts
 $SelCustoLista = "SELECT actiontime, sec_to_time(actiontime) AS Hora,name,cost_time,cost_fixed,cost_material,FORMAT(cost_time,2,'de_DE') AS cost_time2, FORMAT(cost_fixed,2,'de_DE') AS cost_fixed2, FORMAT(cost_material,2,'de_DE') AS cost_material2, SUM(cost_material + cost_fixed + cost_time * actiontime/3600) AS CustoItem FROM glpi_ticketcosts WHERE tickets_id = '".$OsId."' GROUP BY id";
 $ResCustoLista = $DB->query($SelCustoLista);
+// sql coûts total
 $SelCusto = "SELECT SUM(cost_material + cost_fixed + cost_time * actiontime/3600) AS SomaTudo FROM glpi_ticketcosts WHERE tickets_id = '".$OsId."'";
 $ResCusto = $DB->query($SelCusto);
 $Custo = $DB->fetch_assoc($ResCusto);
 $CustoTotal =  $Custo['SomaTudo'];
 $CustoTotalFinal = number_format($CustoTotal, 2, ',', ' ');
+// sql temps total
 $SelTempoTotal = "SELECT SUM(actiontime) AS TempoTotal FROM glpi_ticketcosts WHERE tickets_id = '".$OsId."'";
 $ResTempoTotal = $DB->query($SelTempoTotal);
 $TempoTotal = $DB->fetch_assoc($ResTempoTotal);
@@ -82,6 +96,7 @@ $hours = floor($seconds / 3600);
 $seconds -= $hours * 3600;
 $minutes = floor($seconds / 60);
 $seconds -= $minutes * 60;
+// sql nom localisation
 $SelLocId = "SELECT locations_id FROM `glpi_tickets` WHERE id = '".$OsId."'";
 $ResLocId = $DB->query($SelLocId);
 $LocId = $DB->fetch_assoc($ResLocId);
@@ -89,7 +104,8 @@ $LocationsId = $LocId['locations_id'];
 $SelNameLoc = "SELECT name FROM glpi_locations WHERE id = '".$LocationsId."'";
 $ResNameLoc = $DB->query($SelNameLoc);
 $Loc = $DB->fetch_assoc($ResNameLoc);
-$Locations = $Loc['name']; 
+$Locations = $Loc['name'];
+// sql infos utilisateur
 $SelTicketUsers = "SELECT * FROM glpi_tickets_users WHERE tickets_id = '".$OsId."'";
 $ResTicketUsers = $DB->query($SelTicketUsers);
 $TicketUsers = $DB->fetch_assoc($ResTicketUsers);
@@ -101,9 +117,9 @@ $UserName = $Users['firstname']. " " .$Users['realname'];
 $UserCpf = $Users['registration_number'];
 $UserTelefone = $Users['mobile'];
 $UserEndereco = $Users['comment'];
+$UserCepa = $Users['phone'];
 $UserCep = $Users['phone2'];
 $SelEmail = "SELECT * FROM glpi_useremails WHERE users_id = '".$OsUserId."'";
 $ResEmail = $DB->query($SelEmail);
 $Email = $DB->fetch_assoc($ResEmail);
 $UserEmail = $Email['email'];
-?>
